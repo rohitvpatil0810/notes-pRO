c4d1ce522b76cbdd0d6ec8359badf120b74f79f5
Rohit Patil <rohitvpatil0810@gmail.com>
2024-01-02 21:09:56 +0530

    🚀 Modified Code for Deployment

--- backend/app.js
+++ backend/app.js
@@ -42,10 +42,12 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// if (process.env.NODE_ENV == "production") {
-//   app.use(express.static("client/build"));
-//   const path = require("path");
-//   app.get("*", function (req, res) {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("../client/build"));
+  const path = require("path");
+  app.get("*", function (req, res) {
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
+  });
+}
